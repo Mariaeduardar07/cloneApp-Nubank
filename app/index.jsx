9001f9184029e83c7c631ec91ac5357b3561667a
Sophia Gomes <sophiagomes.tds2.senai@gmail.com>
2025-05-29 11:02:37 -0300

    update: pages accountbalance, index and actionButton

--- app/index.jsx
+++ app/index.jsx
@@ -1,37 +1,32 @@
-import { View, Text, StyleSheet } from 'react-native'
-import AccountBalance from '../components/accountbalance/Accountbalance.js';
-import ActionButton from '../components/actionButton/ActionButton.js';
+import { View, Text, StyleSheet } from "react-native";
+import AccountBalance from "../components/accountbalance/Accountbalance.js";
+import ActionButton from "../components/actionButton/ActionButton.js";
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <ActionButton />
-        <AccountBalance />
-      <Text style={styles.title}>Olá, Usuário</Text>
-      <Text style={styles.subtitle}>Bem-vindo ao seu app Nubank clone</Text>
-
-       
+      <AccountBalance />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#820ad1', // Roxo Nubank
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#820ad1',
+    backgroundColor: "#1c1e21",
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff',
+    fontWeight: "bold",
+    color: "#fff",
   },
   subtitle: {
     fontSize: 16,
-    color: '#fff',
+    color: "#fff",
     marginTop: 8,
   },
-})
-
- 
+});
