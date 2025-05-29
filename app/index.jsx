5baed12fd8f3c52299add39447ca89d408b7ee8e
Maria Eduarda <mariaeduardareistds2.senai@gmail.com>
2025-05-29 11:16:31 -0300

    update

--- app/index.jsx
+++ app/index.jsx
@@ -1,35 +1,34 @@
-import { View, Text, StyleSheet } from 'react-native'
-import AccountBalance from '../components/accountbalance/Accountbalance.js';
+import { View, Text, StyleSheet } from "react-native";
+import AccountBalance from "../components/accountbalance/Accountbalance.js";
+import InfoCard from "../components/infocard/infocard.js";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-        <AccountBalance />
+      <AccountBalance />
+      <InfoCard icon="lock-outline" label="Caixinhas" value="R$ 100,00" />
+      <InfoCard icon="chart-bar" label="Meus Cartões" />
       <Text style={styles.title}>Olá, Usuário</Text>
       <Text style={styles.subtitle}>Bem-vindo ao seu app Nubank clone</Text>
-
-       
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#820ad1', // Roxo Nubank
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#820ad1", // Roxo Nubank
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff',
+    fontWeight: "bold",
+    color: "#fff",
   },
   subtitle: {
     fontSize: 16,
-    color: '#fff',
+    color: "#fff",
     marginTop: 8,
   },
-})
-
- 
+});
---
+++ components/infocard/infocard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const InfoCard = ({ icon, label, value }) => {
+  return (
+    <View style={styles.card}>
+      <View style={styles.left}>
+        <Icon name={icon} size={22} color="#000" style={styles.icon} />
+        <Text style={styles.label}>{label}</Text>
+      </View>
+      <Text style={styles.value}>{value}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#f5f5f5',
+    width: '80%',
+    padding: 12,
+    borderRadius: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  left: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 8,
+  },
+  label: {
+    fontSize: 14,
+    color: '#000',
+  },
+  value: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#000',
+  },
+});
+
+export default InfoCard;
