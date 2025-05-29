ef8eb0d3472139d56ba6a5e3bd8d1927d18d0b6f
Sophia Gomes <sophiagomes.tds2.senai@gmail.com>
2025-05-29 08:50:40 -0300

    feat: create index.jsx and layout.jsx

---
+++ app/index.jsx
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Olá, Usuário</Text>
+      <Text style={styles.subtitle}>Bem-vindo ao seu app Nubank clone</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#820ad1', // Roxo Nubank
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#fff',
+    marginTop: 8,
+  },
+})
